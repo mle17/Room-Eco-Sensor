46ed260ec1884f179a4638dd60f45167b7d3d3c0
Michael Le <michael.vu.le@hotmail.com>
2017-06-02 10:45:05 -0700

    Create ui source and ui header file

---
+++ ui.c
@@ -0,0 +1,14 @@
+/*
+ * ui.c
+ *
+ *      Created on: May 31, 2017
+ *         Authors: Michael Le & Justin L'Heureux
+ *         Project: 45
+ *           Class: CPE 329 - Section 01
+ *         Quarter: Spring 2017
+ *
+ *      Using LCD to display the distance of the ultrasonic sensor
+ *      and whether someone triggered the sensor.
+ */
+
+#include "ui.h"
---
+++ ui.h
@@ -0,0 +1,16 @@
+/*
+ * ui.h
+ *
+ *      Created on: May 31, 2017
+ *         Authors: Michael Le & Justin L'Heureux
+ *         Project: 45
+ *           Class: CPE 329 - Section 01
+ *         Quarter: Spring 2017
+ */
+
+#ifndef UI_H
+#define UI_H
+
+
+
+#endif /* UI_H_ */

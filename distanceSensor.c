1a72ebc215cccea379990b631f99004a2a1cbe50
Michael Le <michael.vu.le@hotmail.com>
2017-06-02 10:46:42 -0700

    Refactor save final distance time

--- distanceSensor.c
+++ distanceSensor.c
@@ -17,7 +17,6 @@
 static int distance_flag = 0;
 
 static unsigned short init_time = 0;
-static unsigned short final_time = 0;
 static unsigned short diff_time = 0;
 
 /*
@@ -65,8 +64,7 @@ void save_init_distance_time(unsigned short time) {
 }
 
 void save_final_distance_time(unsigned short time) {
-    final_time = time;
-    diff_time = final_time - init_time;
+    diff_time = time - init_time;
 }
 
 unsigned int get_distance() {
